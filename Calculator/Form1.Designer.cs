6b554a53c5dd8d2df6efd9487bda37fdb08557ea
Rafael Oliveira <orafael767@gmail.com>
2022-11-22 18:41:23 +0000

    Update on comments

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Variável de designer necessária.
-        /// </summary>
+       
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Limpar os recursos que estão sendo usados.
-        /// </summary>
-        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
